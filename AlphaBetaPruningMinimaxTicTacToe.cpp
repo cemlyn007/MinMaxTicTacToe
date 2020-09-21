fe217450af463a42cd6462a2ff478e4895e120a1
Cemlyn N Waters <cnw119@ic.ac.uk>
2020-09-21 23:42:45 +0100

    Working Alpha-Beta Pruning

--- AlphaBetaPruningMinimaxTicTacToe.cpp
+++ AlphaBetaPruningMinimaxTicTacToe.cpp
@@ -5,6 +5,7 @@
 #include "AlphaBetaPruningMinimaxTicTacToe.h"
 
 #include <utility>
+#include <thread>
 
 AlphaBetaPruningMinimaxTicTacToe::AlphaBetaPruningMinimaxTicTacToe(int m, int n, int k,
                                                                    Player player1, Player player2) : TicTacToe(m, n, k,
@@ -20,35 +21,30 @@ TicTacToe::Score AlphaBetaPruningMinimaxTicTacToe::getMiniMax(const Player &play
     if (gameState == TicTacToe::GameState::RUNNING) {
         Score best_score, score;
         Board::Marker marker = player.getMarker();
-
-//        std::cout << "n_options: " << getOptions().size() << std::endl;
-
-        // This code gets the optimiser and sets the initial best score (the worst case)
         if (marker == PLAYER_MAX.getMarker()) {
             best_score = getWorstCaseScore(PLAYER_MAX.getMarker());
             for (std::tuple<int, int> move: getOptions()) {
                 board.markBoard(move, player.getMarker());
                 score = getMiniMax(prev_player, player, alpha, beta);
                 best_score = (Score)(std::max((int) score, (int) best_score));
-                alpha = (int)(std::max((int) best_score, alpha));
+                alpha = (int) (std::max((int) best_score, alpha));
                 board.unmarkBoard(move);
                 if (beta <= alpha) { break; }
             }
-
+            return best_score;
         } else if (marker == PLAYER_MIN.getMarker()) {
             best_score = getWorstCaseScore(PLAYER_MIN.getMarker());
             for (std::tuple<int, int> move: getOptions()) {
                 board.markBoard(move, player.getMarker());
                 score = getMiniMax(prev_player, player, alpha, beta);
                 best_score = (Score)(std::min((int) score, (int) best_score));
-                beta = (int)(std::min((int) best_score, beta));
+                beta = (int) (std::min((int) best_score, beta));
                 board.unmarkBoard(move);
                 if (beta <= alpha) { break; }
             }
+            return best_score;
         }
-        return best_score;
     } else if (gameState == TicTacToe::GameState::FINISHED) {
-//        showBoard();
         return getScore(prev_player.getMarker());
     }
 }
@@ -60,54 +56,42 @@ AlphaBetaPruningMinimaxTicTacToe::getBestMove(const Player &curr_player, const P
     int alpha, beta;
     std::vector<std::tuple<int, int>> options = getOptions();
     Board::Marker marker = curr_player.getMarker();
-
-    alpha = (int)getScore(PLAYER_MIN.getMarker());
-    beta = (int)getScore(PLAYER_MAX.getMarker());
-
-    // This code gets the optimiser and sets the initial best score (the worst case)
+    alpha = (int) getScore(PLAYER_MIN.getMarker());
+    beta = (int) getScore(PLAYER_MAX.getMarker());
     if (marker == PLAYER_MAX.getMarker()) {
         best_score = getWorstCaseScore(PLAYER_MAX.getMarker());
         for (std::tuple<int, int> move: options) {
             board.markBoard(move, curr_player.getMarker());
             score = getMiniMax(next_player, curr_player, alpha, beta);
             board.unmarkBoard(move);
-            std::cout << "Move: (" << std::get<0>(move) <<", " << std::get<1>(move) << ") " << "Score: " << score << std::endl;
+            std::cout << "Move: (" << std::get<0>(move) << ", " << std::get<1>(move) << ") " << "Score: " << score
+                      << std::endl;
             best_score = (Score)(std::max((int) score, (int) best_score));
             if (score == best_score) { best_move = move; }
-//            alpha = (int)(std::max((int) best_score, (int) alpha));
-//            if (beta <= alpha) { break; }
         }
-
     } else if (marker == PLAYER_MIN.getMarker()) {
         best_score = getWorstCaseScore(PLAYER_MIN.getMarker());
         for (std::tuple<int, int> move: options) {
             board.markBoard(move, curr_player.getMarker());
             score = getMiniMax(next_player, curr_player, alpha, beta);
             board.unmarkBoard(move);
-            std::cout << "Move: (" << std::get<0>(move) <<", " << std::get<1>(move) << ") " << "Score: " << score << std::endl;
+            std::cout << "Move: (" << std::get<0>(move) << ", " << std::get<1>(move) << ") " << "Score: " << score
+                      << std::endl;
             best_score = (Score)(std::min((int) score, (int) best_score));
             if (score == best_score) { best_move = move; }
-//            beta = (int)(std::min((int) best_score, (int) beta));
-//            if (beta <= alpha) { break; }
         }
     }
     return best_move;
 }
 
+
 std::tuple<int, int>
 AlphaBetaPruningMinimaxTicTacToe::getUserInput(const Player &curr_player, const Player &next_player) {
-
     if (curr_player.getMarker() == PLAYER_MIN.getMarker()) {
         std::tuple<int, int> move(getBestMove(curr_player, next_player));
-//        showBoard();
         return move;
     }
-
     getBestMove(curr_player, next_player);
-
-//    showBoard();
-
-    // This is modifiable
     static int a, b;
     if (board.screen == Board::ON) {
         std::tuple<int, int> mouse_position = board.getMousePosition();
@@ -120,7 +104,6 @@ AlphaBetaPruningMinimaxTicTacToe::getUserInput(const Player &curr_player, const 
         std::cout << "Received: " << "(" << a << "," << b << ")" << std::endl;
         return std::tuple<int, int>{a, b};
     }
-
 }
 
 void AlphaBetaPruningMinimaxTicTacToe::play() {
@@ -128,9 +111,7 @@ void AlphaBetaPruningMinimaxTicTacToe::play() {
     Score end_score;
     Player prev_player = PLAYER_MIN;
     Player curr_player = PLAYER_MAX;
-
     showBoard();
-
     while (true) {
         move = getUserInput(curr_player, prev_player);
         board.markBoard(std::get<0>(move), std::get<1>(move), curr_player.getMarker());
